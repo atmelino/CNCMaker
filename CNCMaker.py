33d15247dd5cb3525f8dbaf3b089e6ee656125c4
jatnik <jatnik@jat>
2015-11-20 18:46:42 -0600

    main program

---
+++ CNCMaker.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
+import Tkinter
+
+class simpleapp_tk(Tkinter.Tk):
+    def __init__(self,parent):
+        Tkinter.Tk.__init__(self,parent)
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        self.grid()
+
+        self.entryVariable = Tkinter.StringVar()
+        self.entry = Tkinter.Entry(self,textvariable=self.entryVariable)
+        self.entry.grid(column=0,row=0,sticky='EW')
+        self.entry.bind("<Return>", self.OnPressEnter)
+        self.entryVariable.set(u"Enter text here.")
+
+        button1 = Tkinter.Button(self,text=u"pocket",
+                                command=self.OnButton1Click)
+        button1.grid(column=1,row=0)
+
+        button2 = Tkinter.Button(self,text=u"engrave",
+                                command=self.OnButton2Click)
+        button2.grid(column=1,row=1)
+
+        self.labelVariable = Tkinter.StringVar()
+        label = Tkinter.Label(self,textvariable=self.labelVariable,
+                              anchor="w",fg="white",bg="blue")
+        label.grid(column=0,row=2,columnspan=2,sticky='EW')
+        self.labelVariable.set(u"Hello !")
+
+        self.grid_columnconfigure(0,weight=1)
+        self.resizable(True,False)
+        self.update()
+        self.geometry(self.geometry())       
+        self.entry.focus_set()
+        self.entry.selection_range(0, Tkinter.END)
+
+    def OnButton1Click(self):
+        self.labelVariable.set( self.entryVariable.get()+" pocket" )
+        self.entry.focus_set()
+        self.entry.selection_range(0, Tkinter.END)
+
+    def OnButton2Click(self):
+        self.labelVariable.set( self.entryVariable.get()+" engrave" )
+        self.entry.focus_set()
+        self.entry.selection_range(0, Tkinter.END)
+
+    def OnPressEnter(self,event):
+        self.labelVariable.set( self.entryVariable.get()+" (You pressed ENTER)" )
+        self.entry.focus_set()
+        self.entry.selection_range(0, Tkinter.END)
+
+if __name__ == "__main__":
+    app = simpleapp_tk(None)
+    app.title('CNCMaker')
+    app.mainloop()
